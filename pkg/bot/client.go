a0ab3645864124562ffbea446b8768e2308ac1b2
Aser <aserdipanda@gmail.com>
2022-05-18 00:00:34 +0100

    feat: bot can now download a file

--- pkg/bot/client.go
+++ pkg/bot/client.go
@@ -2,14 +2,24 @@ package bot
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/DipandaAser/tg-bot-storage/pkg/storage"
 	tb "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"io"
+	"net/http"
 )
 
 type Client struct {
 	botToken string
 	b        *tb.BotAPI
+}
+
+type botResult struct {
+	Ok          bool   `json:"ok"`
+	ErrorCode   int    `json:"error_code"`
+	Description string `json:"description"`
 }
 
 func NewClient(botToken string) (*Client, error) {
@@ -45,4 +55,58 @@ func (c *Client) UploadFileReader(chatId int64, fileName string, fileReader io.R
 func (c *Client) UploadFileBuffer(chatId int64, fileName string, fileData []byte) (storage.MessageIdentifier, error) {
 	reader := bytes.NewReader(fileData)
 	return c.UploadFileReader(chatId, fileName, reader)
+}
+
+func (c *Client) DownloadFileReader(identifier storage.MessageIdentifier, copyChat int64) (io.ReadCloser, error) {
+	copiedMessage, err := c.b.Send(tb.ForwardConfig{
+		BaseChat:   tb.BaseChat{ChatID: copyChat},
+		FromChatID: identifier.ChatId,
+		MessageID:  identifier.MessageId,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if copiedMessage.Document == nil {
+		return nil, errors.New("message doesn't contain document")
+	}
+
+	url, err := c.b.GetFileDirectURL(copiedMessage.Document.FileID)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		err = fmt.Errorf("request fail with status code: %v, %s", response.StatusCode, response.Status)
+		// read json error into the botResult struct
+		var res botResult
+		if json.NewDecoder(response.Body).Decode(&res) != nil {
+			return nil, err
+		}
+
+		err = fmt.Errorf("code: %v, message, %s", res.ErrorCode, res.Description)
+	}
+
+	return response.Body, nil
+}
+
+func (c *Client) DownloadFileBuffer(identifier storage.MessageIdentifier, copyChat int64) ([]byte, error) {
+	reader, err := c.DownloadFileReader(identifier, copyChat)
+	if err != nil {
+		return nil, err
+	}
+
+	defer reader.Close()
+
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(reader); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -6,6 +6,6 @@ import "io"
 type Storage interface {
 	UploadFileBuffer(int64, string, []byte) (MessageIdentifier, error)
 	UploadFileReader(int64, string, io.Reader) (MessageIdentifier, error)
-	DownloadFileBuffer(identifier MessageIdentifier) ([]byte, error)
-	DownloadFileReader(identifier MessageIdentifier) (io.ReadCloser, error)
+	DownloadFileBuffer(identifier MessageIdentifier, copyChat int64) ([]byte, error)
+	DownloadFileReader(identifier MessageIdentifier, copyChat int64) (io.ReadCloser, error)
 }
