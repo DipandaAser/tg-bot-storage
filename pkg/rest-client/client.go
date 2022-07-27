cd999d1bd1e168b9c2acd46325021457f8fd2605
Aser <aserdipanda@gmail.com>
2022-07-27 16:45:59 +0100

    fix: url path

--- pkg/rest-client/client.go
+++ pkg/rest-client/client.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	v1 "github.com/DipandaAser/tg-bot-storage/pkg/models/v1"
 	"io"
+	"mime"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -18,12 +19,20 @@ type RestClient struct {
 	client http.Client
 }
 
-func NewRestClient(apikey, apiurl string) RestClient {
+type apiError struct {
+	Error string `json:"error"`
+}
+
+func NewRestClient(apiUrl, apiKey string) RestClient {
 	return RestClient{
-		apiKey: apikey,
-		apiUrl: strings.TrimSuffix(apiurl, "/"),
+		apiKey: apiKey,
+		apiUrl: strings.TrimSuffix(apiUrl, "/"),
 		client: http.Client{},
 	}
+}
+
+func (rc *RestClient) getApiUrl() string {
+	return rc.apiUrl + "/api"
 }
 
 func (rc *RestClient) UploadFileReader(chatId int64, fileName string, fileReader io.Reader) (v1.MessageIdentifier, error) {
@@ -31,14 +40,18 @@ func (rc *RestClient) UploadFileReader(chatId int64, fileName string, fileReader
 	params.Set("chat_id", fmt.Sprintf("%d", chatId))
 	params.Set("file_name", fileName)
 	params.Set("api-key", rc.apiKey)
-	var finalUrl string = fmt.Sprintf("%s/%s?%s", rc.apiUrl, "files", params.Encode())
+	var finalUrl = fmt.Sprintf("%s/%s?%s", rc.getApiUrl(), "files", params.Encode())
 	response, err := rc.client.Post(finalUrl, "application/octet-stream", fileReader)
 	if err != nil {
 		return v1.MessageIdentifier{}, err
 	}
 	defer response.Body.Close()
 	if response.StatusCode != http.StatusOK {
-		return v1.MessageIdentifier{}, fmt.Errorf("could not upload file. status code: %d", response.StatusCode)
+		var apiErr apiError
+		if err := json.NewDecoder(response.Body).Decode(&apiErr); err != nil {
+			return v1.MessageIdentifier{}, fmt.Errorf("could not upload file. status code: %d", response.StatusCode)
+		}
+		return v1.MessageIdentifier{}, fmt.Errorf("could not upload file. status code: %d, error: %s", response.StatusCode, apiErr.Error)
 	}
 	var fileIdentifier v1.MessageIdentifier
 	err = json.NewDecoder(response.Body).Decode(&fileIdentifier)
@@ -56,27 +69,33 @@ func (rc *RestClient) UploadFileBuffer(chatId int64, fileName string, fileData [
 func (rc *RestClient) DownloadFileReader(identifier v1.MessageIdentifier, copyChat int64) (*v1.DownloadReaderResult, error) {
 	params := url.Values{}
 	params.Set("chat_id", fmt.Sprintf("%d", identifier.ChatId))
-	params.Set("message_id", fmt.Sprintf("%d", identifier.MessageId))
+	params.Set("msg_id", fmt.Sprintf("%d", identifier.MessageId))
 	params.Set("draft_chat_id", fmt.Sprintf("%d", copyChat))
 	params.Set("api-key", rc.apiKey)
-	var finalUrl string = fmt.Sprintf("%s/%s?%s", rc.apiUrl, "files", params.Encode())
+	var finalUrl = fmt.Sprintf("%s/%s?%s", rc.getApiUrl(), "files", params.Encode())
 	response, err := rc.client.Get(finalUrl)
 	if err != nil {
 		return nil, err
 	}
 	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("could not download file. status code: %d", response.StatusCode)
+		var apiErr apiError
+		if err := json.NewDecoder(response.Body).Decode(&apiErr); err != nil {
+			return nil, fmt.Errorf("could not upload file. status code: %d", response.StatusCode)
+		}
+		return nil, fmt.Errorf("could not upload file. status code: %d, error: %s", response.StatusCode, apiErr.Error)
 	}
 	// get the file size in header content-length and convert it to int64
 	var fileSize int64 = 0
 	if contentLength := response.Header.Get("Content-Length"); contentLength != "" {
 		fileSize, _ = strconv.ParseInt(contentLength, 10, 64)
 	}
+
+	_, contentDispParams, err := mime.ParseMediaType(response.Header.Get("Content-Disposition"))
 	return &v1.DownloadReaderResult{
 		Data: response.Body,
 		FileInfo: v1.FileInfo{
 			Size:        fileSize,
-			Name:        response.Header.Get("Content-Disposition"),
+			Name:        contentDispParams["filename"],
 			ContentType: response.Header.Get("Content-Type"),
 		},
 	}, nil
